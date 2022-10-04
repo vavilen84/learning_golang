f2fe8d876d506ad262ee90136e295888a34fa7bc
Vladimir Teplov <vladimir.teplov@gmail.com>
2022-10-04 10:49:42 +0300

    fix

--- topic_5/reflect/value/value.go
+++ topic_5/reflect/value/value.go
@@ -8,8 +8,12 @@ import (
 func main() {
 	var x float64 = 3.4
 	fmt.Println("x value: ", reflect.ValueOf(x))
-	fmt.Println("x value: ", reflect.ValueOf(x).Float())
+	fmt.Printf("Value %+v \r\n", reflect.ValueOf(x))
+	fmt.Printf("Value Type %T \r\n", reflect.ValueOf(x))
+	fmt.Println("x Float value: ", reflect.ValueOf(x).Float())
+	fmt.Printf("Value Float Type %T \r\n", reflect.ValueOf(x).Float())
 	fmt.Println("x value type: ", reflect.ValueOf(x).Type())
+	fmt.Printf("Value Type obj %T \r\n", reflect.ValueOf(x).Type())
 	fmt.Println("x value kind: ", reflect.ValueOf(x).Kind())
 
 	y, ok := reflect.ValueOf(x).Interface().(float64)
